c4c7f4590514dd9a7eaedfdc7b95b3c3f802f2ff
Akash Bhowal <akash.aug10@gmail.com>
2023-01-28 22:00:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+onst alphanumericOnly = str.toLowerCase().match(/[a-z0-9]/g);
+    // 2) Strip out non-alphanumeric characters
+    
+// 3) return string === reversedString
+return alphanumericOnly.join('') === alphanumericOnly.reverse().join('');
+    
+}
+console.log(palindrome("racecar"));
 
 }
 module.exports = palindrome
